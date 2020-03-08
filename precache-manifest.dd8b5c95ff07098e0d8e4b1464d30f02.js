1511f43679a91f4cb3956bf0fb4e1ab518c785c7
ilya1995 <kovalyov.king@yandex.ru>
2020-03-09 01:03:56 +0400

    Updates

---
+++ precache-manifest.dd8b5c95ff07098e0d8e4b1464d30f02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8030b04f57aab50c5a3875b5a2df43dd",
+    "url": "/purchases/index.html"
+  },
+  {
+    "revision": "b84775dd04aad93204fa",
+    "url": "/purchases/static/css/main.410d61a8.chunk.css"
+  },
+  {
+    "revision": "b6a9a8b3e5e15f8bd611",
+    "url": "/purchases/static/js/2.245614a6.chunk.js"
+  },
+  {
+    "revision": "a5264cfddb55a14e6d0d97f3613f825d",
+    "url": "/purchases/static/js/2.245614a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84775dd04aad93204fa",
+    "url": "/purchases/static/js/main.1dae14b1.chunk.js"
+  },
+  {
+    "revision": "4e8b4f66aff8efd58ea1",
+    "url": "/purchases/static/js/runtime-main.6bf55a43.js"
+  }
+]);
