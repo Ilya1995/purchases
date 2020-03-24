bf865ddd2ee6f00fd3afa1e2daf349f24b2248b6
ilya1995 <kovalyov.king@yandex.ru>
2020-03-25 00:37:57 +0400

    type rootReducer

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 <h3>Список покупок</h3>
 <p>React + Redux + TS + Firebase</p>
-<p><a href="https://ilya1995.github.io/purchases/">Тут</a> можно посмотреть</p>
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,27 @@
 import { combineReducers } from 'redux';
 import { users } from '../reducers/users';
-import { firebaseReducer } from 'react-redux-firebase';
+import { InitialStateUsersType, UserType, ItemType } from '../types';
+import { firebaseReducer, FirebaseReducer } from 'react-redux-firebase';
 
-export const rootReducer: any = combineReducers({
-  users: users,
+type ProfileType = {
+  isLoaded: boolean;
+  isEmpty: boolean;
+};
+
+type DBSchema = {
+  users: UserType;
+  purchases: ItemType;
+  others: ItemType;
+  [name: string]: any;
+};
+
+type RootState = {
+  users: InitialStateUsersType;
+  firebase: FirebaseReducer.Reducer<ProfileType, DBSchema>;
+};
+
+export const rootReducer = combineReducers<RootState>({
+  users,
   firebase: firebaseReducer
 });
 
